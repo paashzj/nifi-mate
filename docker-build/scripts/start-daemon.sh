56eeceb9c2b0d8835f63690bbd95891c275ced90
lovehzj <1922919664@qq.com>
2022-01-09 15:30:52 +0800

    use relative path in start script

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -2,4 +2,5 @@
 
 mkdir $NIFI_HOME/logs
 
-java -Xmx128M -XX:MaxDirectMemorySize=32M -jar /opt/sh/nifi/mate/nifi-mate.jar >>$NIFI_HOME/nifi_mate.stdout.log 2>>$NIFI_HOME/nifi_mate.stderr.log
+java -Xmx128M -XX:MaxDirectMemorySize=32M -jar $NIFI_HOME/mate/nifi-mate.jar >>$NIFI_HOME/nifi_mate.stdout.log 2>>$NIFI_HOME/nifi_mate.stderr.log
+
