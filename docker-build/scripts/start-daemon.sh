966f9b30fe9a4bf3b10fd7498a8be6dfc952d61c
lovehzj <1922919664@qq.com>
2022-01-13 23:13:31 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir $NIFI_HOME/logs
+mkdir -p $NIFI_HOME/logs
 
 java -Xmx128M -XX:MaxDirectMemorySize=32M -jar $NIFI_HOME/mate/nifi-mate.jar >>$NIFI_HOME/nifi_mate.stdout.log 2>>$NIFI_HOME/nifi_mate.stderr.log
 
